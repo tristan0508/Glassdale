c025847f13aedbb6055349a18f74f08d6984b562
Tristanwyatt <tristan7306@gmail.com>
2020-09-02 11:44:21 -0500

    drop down sort

--- scripts/convictions/ConvictionProvider.js
+++ scripts/convictions/ConvictionProvider.js
@@ -8,3 +8,4 @@ export const getConviction = async() => {
     conviction = response;
     return conviction;
 };
+
--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -1,18 +1,38 @@
 import {useConviction, getConviction} from './ConvictionProvider.js';
 const convictionElement = document.querySelector('.filters-crime');
+const eventHub = document.querySelector('.container')
+
+
+eventHub.addEventListener("change", event => {
+    if (event.target.id === "crimeSelect") {
+        const customEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeThatWasChosen: event.target.value
+            }
+        })
+    eventHub.dispatchEvent(customEvent)
+    }
+})
+
+
+const render = convictionsCollection => {
+    convictionElement.innerHTML = `
+        <select class="dropdown" id="crimeSelect">
+            <option value="0">Please select a crime...</option>
+            ${
+                convictionsCollection.map(conviction => {
+                    return `<option>${conviction.name}</option>`
+                })
+            }
+        </select>
+    `
+}
+
 
 export const ConvictionSelect = () => {
     getConviction()
-    .then(() => {
-        const convictions = useConviction();
-        convictionElement.innerHTML =
-    `<select class="dropdown" id="crimeSelect">
-        <option value="0">Please select a crime...</option>
-        ${
-            convictions.map(conviction => {
-                return `<option>${conviction.name}</option>`
-            })
-        };
-    </select>`
-});
-};
+        .then(() => {
+            const convictions = useConviction()
+            render(convictions)
+        })
+}

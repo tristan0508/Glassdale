c0eb7a47b7db1ce5a483cce03fbb59b9187f2b98
Tristanwyatt <tristan7306@gmail.com>
2020-09-07 21:01:28 -0500

    fixed sort method

--- scripts/officers/OfficerProvider.js
+++ scripts/officers/OfficerProvider.js
@@ -1,8 +1,19 @@
 let officers = []
 
 export const useOfficers = () => {
-    return officers.slice()
+    let sortOfficers = officers.slice();
+    officers = sortOfficers.sort((a,b) => {
+        if(a.name < b.name){
+            return -1;
+        }else if(a.name > b.name){
+            return 1;
+        }else{
+            return 0;
+        }
+    })
+    return officers;
 }
+
 
 export const getOfficers = () => {
     return fetch("https://criminals.glassdale.us/officers")

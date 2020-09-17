3eb9822f46589d4971a3e5bb83dd8632963a06a2
Tristanwyatt <tristan7306@gmail.com>
2020-09-17 16:02:59 -0500

    notes

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,25 +1,34 @@
 import {getNotes, sliceNotes} from './NoteProvider.js';
 import {htmlNotes} from './Note.js';
+import { criminalReturn, getCriminals } from "../criminals/CriminalProvider.js";
 const notesDisplayElement = document.querySelector("#notesContainer")
 const eventHub = document.querySelector(".container")
 
 
-eventHub.addEventListener("noteStateChanged", () => {
-    noteList();
-});
+
+const addNotesToDom = (notes, suspects) => {
+  
+   notesDisplayElement.innerHTML = notes.map((noteObject) => {
+       noteObject.suspectObj = suspects.find(suspect => {
+           return suspect.id === parseInt(noteObject.suspectId)
+       })
+       return htmlNotes(noteObject)
+   })
+};
 
 
 
 export const noteList = () => {
     getNotes()
+    .then(getCriminals)
     .then(() => {
         const notes = sliceNotes();
-        addNotesToDom(notes);
-        });
+        const suspects = criminalReturn()
+        addNotesToDom(notes, suspects)
+    })
+}
 
-};
-
-const addNotesToDom = (notesPassed) => {
-    let HTML = htmlNotes(notesPassed);
-    notesDisplayElement.innerHTML = HTML;
-};
+eventHub.addEventListener("noteStateChanged", () => {
+    const newNotes = sliceNotes()
+    addNotesToDom(newNotes, criminalReturn())
+});
